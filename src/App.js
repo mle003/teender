99c7b3ceb8131714f1192a2fce12ab2c02b291f6
tahaphuong <phuonghata1995@gmail.com>
2020-07-23 21:39:05 +0700

    hello github

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { Card, CardWrapper } from 'react-swipeable-cards';
+import logo from './assets/logo.png'
+// style
+import './style/main.scss'
+
+ 
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      navTitles: ['Match', 'Message'],
+      chosenIndex: 0,
+      arrowMargin: 5
+    }
+  }
+
+  genTitles(titles, chosenIndex) {
+    let list = titles.map((item, index) => {
+      return <div 
+        onClick={
+          () => {
+            if (chosenIndex != index) 
+              this.setState({chosenIndex: index})
+          }
+        }
+        key={index} 
+        className={['nav-title', index == chosenIndex ? 'chosen-nav-title' : 'unchosen-nav-title'].join(' ')}>
+          {item}
+      </div>
+    })
+    return list
+  }
+
+  render() {
+    let { navTitles, chosenIndex } = this.state
+    return(
+      <div id="main-screen">
+        <div id="nav">
+          <div id="nav-logo"><img src={logo} height="30"/></div>
+          <div id="nav-body">
+            <div id="nav-titles">
+              {this.genTitles(navTitles, chosenIndex)}
+            </div>
+            <div id="nav-main"></div>
+          </div>
+          <div id="nav-footer">
+            <div id="nav-footer-avatar"></div>
+            <div id="nav-footer-text" 
+              onMouseOver={() => this.setState({...this.state, arrowMargin: 12})}
+              onMouseOut={() => this.setState({...this.state, arrowMargin: 5})}>
+                To my profile 
+                <span style={{marginLeft: this.state.arrowMargin}}>➝</span>
+            </div>
+          </div>
+        </div>
+        <div id="main">
+          <div id="card-wrapper">
+            <CardWrapper>
+              <Card>Hello World 1!</Card>
+              <Card>Hello World 2!</Card>
+            </CardWrapper>
+          </div>
+          <div id="instruction"></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+// const isRunningDevServer = process.env.WEBPACK_DEV_SERVER
+module.exports = {
+  entry: path.join(__dirname, 'src/main.js'),
+  output: {
+    filename: 'js/[name].[contenthash].js',
+    publicPath: '/'
+  },
+  plugins: [
+    // ...(
+    //   isRunningDevServer
+    //   ? []
+    //   : 
+    // ),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src/index.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash].css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ]
+      }, 
+      {
+        test: /\.(png|jpg|jpeg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[contenthash].[ext]',
+          outputPath: 'assets/img'
+        }
+      }
+    ]
+  }
+}
